496ed427e04d465accd516e506a6dcb90feede08
Daniel Coulbourne <daniel.coulbourne@gmail.com>
2017-04-24 20:07:00 -0400

    Update root template with link to GH.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 $app->get('/', function () use ($app) {
-    return 'Append an iTunes id to get an array of episode slugs. Example: <a href="1169565091">Fits + Starts</a>';
+    return 'Append an iTunes id to get an array of Overcast.fm episode slugs. Example: <a href="1169565091">Fits + Starts</a>. Repository: <a href="https://github.com/DanielCoulbourne/Overcast-Episode-Slug-Retriever">Github</a>';
 });
 
 $app->get('/{id}', 'ShowController@index');
